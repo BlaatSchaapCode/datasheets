4cde32eaecf063f4ac982010f981c99470555a58
André van Schoubroeck <andre@blaatschaap.be>
2022-11-27 21:52:21 +0100

    Adding datasheets

---
+++ rfid/LF/Reader/Triades - Serial protocol for a chinese RFID 125Khz reader_writer_bestanden/caption.js
@@ -0,0 +1,40 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*
+        GNU General Public License version 2 or later; see LICENSE.txt
+*/
+var JCaption=function(c){var e,b,a=function(f){e=jQuery.noConflict();b=f;e(b).each(function(g,h){d(h)})},d=function(i){var h=e(i),f=h.attr("title"),j=h.attr("width")||i.width,l=h.attr("align")||h.css("float")||i.style.styleFloat||"none",g=e("<p/>",{text:f,"class":b.replace(".","_")}),k=e("<div/>",{"class":b.replace(".","_")+" "+l,css:{"float":l,width:j}});h.before(k);k.append(h);if(f!==""){k.append(g)}};a(c)};
+
+
+}
+/*
+     FILE ARCHIVED ON 15:58:28 Jun 30, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:15:55 Sep 12, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 315.65
+  exclusion.robots: 0.221
+  exclusion.robots.policy: 0.203
+  cdx.remote: 0.136
+  esindex: 0.017
+  LoadShardBlock: 30.622 (3)
+  PetaboxLoader3.datanode: 41.782 (4)
+  CDXLines.iter: 17.209 (3)
+  load_resource: 100.471
+  PetaboxLoader3.resolve: 55.51
+*/
---
+++ rfid/LF/Reader/Triades - Serial protocol for a chinese RFID 125Khz reader_writer_bestanden/jquery-noconflict.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+jQuery.noConflict();
+
+
+}
+/*
+     FILE ARCHIVED ON 04:14:30 May 01, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:16:02 Sep 12, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 3829.974
+  exclusion.robots: 0.146
+  exclusion.robots.policy: 0.135
+  RedisCDXSource: 1.85
+  esindex: 0.012
+  LoadShardBlock: 3807.675 (3)
+  PetaboxLoader3.datanode: 3835.703 (4)
+  CDXLines.iter: 17.072 (3)
+  load_resource: 4100.89
+  PetaboxLoader3.resolve: 4056.869
+*/
